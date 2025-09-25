41853a886b96bdc0f3114e44ce2af0fa1dee4463
galangMaheswara <ullyses88@gmail.com>
2025-09-25 09:06:29 +0700

    perubahan 2

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, Ini baris kode pertama");
         System.out.println("Ini galang yang menulis");
+        System.out.println("Galang sedang mengerjakan tugas praktikum daspro");
     }
 }
