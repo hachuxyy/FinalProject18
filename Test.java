4769e8a374c4fab81c996e726238df6a2f2e5ae9
galangMaheswara <ullyses88@gmail.com>
2025-09-25 09:30:41 +0700

    perubahan oleh Galang

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test {
         System.out.println("Hi, Ini baris kode pertama");
         System.out.println("Ini galang yang menulis");
         System.out.println("Galang sedang mengerjakan tugas praktikum daspro");
-        System.out.println("Haqi Melakukan Penambahan Code");
+        System.out.println("Haqi Melakukan Penambahan ");
     }
 }
